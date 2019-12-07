eb677a46efa44cf496d20cdb3ba14e4cc3adc0b6
Emanuel Ciuca <emanuel.ciuca@gmail.com>
2019-12-07 14:49:59 +0200

    Added spring initalizr files

---
+++ src/main/java/com/emanuelciuca/trainings/projects/advertisements/AdvertisementsApplication.java
@@ -0,0 +1,13 @@
+package com.emanuelciuca.trainings.projects.advertisements;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvertisementsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvertisementsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emanuelciuca/trainings/projects/advertisements/AdvertisementsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emanuelciuca.trainings.projects.advertisements;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvertisementsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
