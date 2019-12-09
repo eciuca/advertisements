a4a4ecf8b78bf6af3f58d69c6e3b8f9787616a22
Emanuel Ciuca <emanuel.ciuca@gmail.com>
2019-12-09 09:11:21 +0200

    Create advertisement API unhappy test

--- src/test/java/com/emanuelciuca/trainings/projects/advertisements/controller/AdvertisementsControllerRestIntegrationTest.java
+++ src/test/java/com/emanuelciuca/trainings/projects/advertisements/controller/AdvertisementsControllerRestIntegrationTest.java
@@ -5,12 +5,16 @@ import com.emanuelciuca.trainings.projects.advertisements.dto.AdvertisementDto;
 import com.emanuelciuca.trainings.projects.advertisements.model.Advertisement;
 import com.emanuelciuca.trainings.projects.advertisements.repository.AdvertisementsRepository;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import java.util.Optional;
+
+import static com.emanuelciuca.trainings.projects.advertisements.controller.AdvertisementsController.API_ADVERTISEMENTS;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
@@ -38,7 +42,7 @@ public class AdvertisementsControllerRestIntegrationTest extends RestIntegration
                 .withId(expectedAdvertisement.getId())
                 .withTitle(expectedAdvertisement.getTitle());
 
-        String relativePath = AdvertisementsController.API_ADVERTISEMENTS + "/" + expectedAdvertisement.getId();
+        String relativePath = API_ADVERTISEMENTS + "/" + expectedAdvertisement.getId();
         ResponseEntity<AdvertisementDto> response = this.restTemplate
                 .getForEntity(url(relativePath), AdvertisementDto.class);
 
@@ -51,12 +55,30 @@ public class AdvertisementsControllerRestIntegrationTest extends RestIntegration
     public void givenNonExistingId_whenGetAdvertisementById_thenStatusCodeIsInternalServerError() {
         long nonExistingId = 1L;
 
-        String relativePath = AdvertisementsController.API_ADVERTISEMENTS + "/" + nonExistingId;
+        String relativePath = API_ADVERTISEMENTS + "/" + nonExistingId;
 
         HttpStatus statusCode = this.restTemplate
                 .getForEntity(url(relativePath), AdvertisementDto.class)
                 .getStatusCode();
 
         assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, statusCode);
+    }
+
+    @Test
+    public void givenAdvertisementDetails_whenPostRequestIsReceived_ThenCreateNewAdvertisement() {
+        AdvertisementDto details = AdvertisementDto.advertisementDto()
+                .withTitle("new advertisement");
+
+        ResponseEntity<AdvertisementDto> response = this.restTemplate
+                .postForEntity(API_ADVERTISEMENTS, details, AdvertisementDto.class);
+
+        AdvertisementDto newAdvertisementDto = AdvertisementDto.advertisementDto()
+                .withId(response.getBody().id)
+                .withTitle(response.getBody().title);
+
+        Optional<Advertisement> newAdvertisement = this.repository.findById(newAdvertisementDto.id);
+
+        Assertions.assertTrue(newAdvertisement.isPresent());
+        Assertions.assertEquals(details, newAdvertisementDto);
     }
 }
